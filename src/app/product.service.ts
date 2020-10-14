d6835189747c25596aa1cac75b39d1e3425f1ad6
Darlington Simeon <dardinho@ymail.com>
2020-10-14 16:04:34 +0100

    update product service file

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response} from '@angular/http';
+import { Album } from './album';
 import {Observable} from 'rxjs/Observable'
+import { Product } from './product';
 
 import 'rxjs/add/operator/map'
 
@@ -10,11 +12,14 @@ export class ProductService {
   private _productsUrl = '../assets/products.json';
   constructor(private _http: Http) { }
 
-  getAlbum(id:number){
+  getAlbum(id:number):Observable<Album>{
     return this._http.get(this._albumUrl).map((response)=>
-      response.json()
+      <Album>response.json()
     );
   }
- 
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response)=>
+      <Product[]>response.json()
+    ); 
   }
 }
